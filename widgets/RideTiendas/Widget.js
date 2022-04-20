9a987a46b878ced522bef237ecebeace64d89e9d
igcalahorro <98539804+igcalahorro@users.noreply.github.com>
2022-04-20 19:35:01 +0200

    Update Widget.js

--- widgets/RideTiendas/Widget.js
+++ widgets/RideTiendas/Widget.js
@@ -38,10 +38,10 @@ define(['dojo/_base/declare', 'jimu/BaseWidget',
         this.symbol = new SimpleMarkerSymbol();
         var marker = this.symbol;
         var line = new SimpleLineSymbol();
-        line.setColor(new Color([38, 115, 0, 1]));
-        marker.setSize(25);
+        line.setColor(new Color([133, 133, 133, 1]));
+        marker.setSize(30);
         marker.setOutline(line);
-        marker.setColor(new Color([56, 168, 0, 0.88]));
+        marker.setColor(new Color([255, 255, 0, 0.88]));
         marker.setPath("M16,3.5c-4.142,0-7.5,3.358-7.5,7.5c0,4.143,7.5,18.121,7.5,18.121S23.5,15.143,23.5,11C23.5,6.858,20.143,3.5,16,3.5z M16,14.584c-1.979,0-3.584-1.604-3.584-3.584S14.021,7.416,16,7.416S19.584,9.021,19.584,11S17.979,14.584,16,14.584z");
         marker.setStyle(SimpleMarkerSymbol.STYLE_PATH);
 
